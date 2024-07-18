017de5bffd4ee134425bb47f0334677ad9abf1fa
preeti2239 <prpal@digisprint.com>
2024-07-18 11:48:33 +0530

    Created email services

---
+++ src/main/java/com/digisprint/EmailUtils/EmailService.java
@@ -0,0 +1,33 @@
+package com.digisprint.EmailUtils;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailService {
+
+	private JavaMailSender javaMailSender;
+
+	public EmailService(JavaMailSender javaMailSender) {
+		super();
+		this.javaMailSender = javaMailSender;
+	}
+	
+	@Async
+	public String MailSendingService(String fromEmail, String toEmail, String bodyOfMail, String subjectOfMail) {
+		
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setFrom(fromEmail);
+		message.setTo(toEmail);
+		message.setText(bodyOfMail);
+		message.setSubject(subjectOfMail);
+		
+		javaMailSender.send(message);
+		
+		return "Mail Sent";
+		
+	}
+	
+}
