143659b63112bb9b02a726a00e6503a768fb66a1
preeti2239 <prpal@digisprint.com>
2024-08-07 12:37:39 +0530

    removed conflicts

--- src/main/java/com/digisprint/controller/AccessBeanController.java
+++ src/main/java/com/digisprint/controller/AccessBeanController.java
@@ -1,5 +1,102 @@
 package com.digisprint.controller;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import javax.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.digisprint.bean.AccessBean;
+import com.digisprint.service.AccessBeanService;
+import com.digisprint.utils.ApplicationConstants;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@RestController
+//@RequestMapping(ApplicationConstants.INTERNAL_USERS)
+@Tag(name= ApplicationConstants.ROLE_MANAGEMENT)
+@CrossOrigin
 public class AccessBeanController {
 
+	private AccessBeanService accessBeanService;
+	
+	public AccessBeanController(AccessBeanService accessBeanService) {
+		super();
+		this.accessBeanService = accessBeanService;
+	}
+
+	@Operation(summary = "Method Used for LogIn")
+	@PostMapping("/login")
+	String login(@RequestParam String userName, @RequestParam String password) {
+		return accessBeanService.login(userName, password);
+	}
+	
+	@Operation(summary = "Method is used for LogIn with Token")
+	@PostMapping("/loginWithToken")
+	ResponseEntity validateAndGenerateToken(@RequestParam String token) {
+		return accessBeanService.validateAndGenerateToken(token);
+	}
+	
+	@Operation(summary="This method is used to save users")
+	@PostMapping("/save")
+	ResponseEntity saveInternalUsers(@Valid @RequestBody AccessBean accessBean) {
+		 return accessBeanService.saveInternalUsers(accessBean);	
+	}
+	
+	@Operation(summary=" This method is used to get all the internal users")
+	@GetMapping("/getAll")
+	ResponseEntity getAllInternalUsers() {
+		return accessBeanService.getAllInternalUsers();
+	}
+	
+	@Operation(summary ="This method is used to get internalusers")
+	@GetMapping("/fetchUsersById/{id}")
+	ResponseEntity fetchInternalUsersById(@PathVariable("id") String id) {
+		return accessBeanService.fetchInternalUsersById(id);
+	}
+	
+	@Operation(summary = "This method is used to remove internal users")
+	@DeleteMapping("/removeAccess/{id}")
+	ResponseEntity softDeleteInternalUsers(@PathVariable("id") String id) {
+		return accessBeanService.softDeleteInternalUsers(id);
+	}
+	@PostMapping(value="/uploadEventsImages",consumes = { "multipart/form-data" })
+	public ResponseEntity uploadEventsAnnocementsImages(@RequestParam(name="events",required =false) MultipartFile events,
+			@RequestParam(name="imagesForHomePage",required =false) MultipartFile imagesForHomePage ,
+		@RequestParam(name="title",required=false) String title,
+		@RequestParam(name="description",required=false) String description) throws IOException{
+		return accessBeanService.uploadEventsAnnocementsImages(events, imagesForHomePage, title, description);
+	}
+	
+	@PostMapping(value="/postingAnnouncements")
+	public ResponseEntity postingAnnouncements(@RequestParam String title, @RequestParam String description)	{
+		return accessBeanService.postingAnnouncements(title,description);
+	}
+	
+	@GetMapping(value="/getAllAnnouncements")
+	public ResponseEntity getAllAnnouncement () {
+		return accessBeanService.getAllAnnouncement();
+	}
+	
+	@GetMapping(value="/getEvents")
+	public ResponseEntity getEvents() throws MalformedURLException {
+		return accessBeanService.getEvents();
+	}
+	
+	@GetMapping(value="/getImages")
+	public ResponseEntity getImages() throws MalformedURLException {
+		return accessBeanService.getImages();
+	}
+	
 }
--- src/main/java/com/digisprint/controller/RegistrationController.java
+++ src/main/java/com/digisprint/controller/RegistrationController.java
@@ -1,5 +1,123 @@
 package com.digisprint.controller;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.http.HttpHeaders;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+import javax.mail.MessagingException;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.data.domain.Page;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.digisprint.bean.PaymentInfo;
+import com.digisprint.bean.ProgressBarReport;
+import com.digisprint.bean.RegistrationFrom;
+import com.digisprint.exception.UserNotFoundException;
+import com.digisprint.requestBean.ApprovalFrom;
+import com.digisprint.requestBean.RegistrationFrom2;
+import com.digisprint.service.RegistrationService;
+import com.digisprint.utils.ApplicationConstants;
+
+import io.swagger.v3.oas.annotations.Operation;
+
+@RestController
+@RequestMapping(ApplicationConstants.USER_API)
+@CrossOrigin
 public class RegistrationController {
 
+     private RegistrationService registrationService;
+	
+	public RegistrationController(RegistrationService registrationService) {
+		super();
+		this.registrationService = registrationService;
+	}
+
+	@Autowired
+	private HttpServletRequest request;
+	
+	 public String getToken() {
+	        return request.getHeader("token");	        
+	    }
+	
+	@Operation(summary="This method is used for 1st level of Registration")
+	@PostMapping("/register")
+	public RegistrationFrom registerUser(@Validated @RequestBody RegistrationFrom from) throws IOException, MessagingException {
+		
+		return this.registrationService.registerUser(from) ;
+	}
+
+	@Operation(summary = "This method is used to upload files for user, aadhar, voter id and caste certificate and profile")
+	@PostMapping(value = "/uploadByUserId/{userId}", consumes = { "multipart/form-data" })
+	public ResponseEntity uploadFile(@PathVariable String userId, @RequestParam(name="aadharCard",required =false) MultipartFile aadharCard,
+			@RequestParam(name="voterIdCard",required =false) MultipartFile voterIdCard,
+			@RequestParam(name="profilePic",required =false) MultipartFile profilePic) throws Exception {
+		return registrationService.upload(userId, aadharCard, voterIdCard, profilePic);
+	} 
+	
+	@Operation(summary= "This method is used to get all users ")
+	@GetMapping(value="/getAllUsers")
+	public Page<RegistrationFrom> getAllRegisteredUsers( @RequestParam(defaultValue = "0") int page,
+			 @RequestParam(defaultValue = "10") int size) {
+		return registrationService.getAllRegisteredUsers(page,size);
+	}
+	
+	@Operation(summary="")
+	@PostMapping(value="/approval/{userId}")
+	public void committeePresidentAccountantApproval(@PathVariable(value="userId") String userId,
+			@RequestBody ApprovalFrom approvalFrom) throws Exception {
+		 registrationService.committeePresidentAccountantApproval(getToken(),userId,approvalFrom );
+	}
+	
+	@Operation(summary="")
+	@PostMapping(value="/progressBar/{userId}")
+	public ProgressBarReport progressBarForAUser(@RequestParam(value="userId") String userId) {
+		return registrationService.progressBarForAUser(userId);
+	}
+	
+	@Operation(summary="")
+	@PostMapping(value="/registrationThreeForm")
+	public RegistrationFrom userFillingRegistrationThreeForm(@RequestBody RegistrationFrom2 registrationFrom2) {
+		return registrationService.userFillingRegistrationThreeForm(getToken(),registrationFrom2 );
+	}
+	
+	@Operation(summary="")
+	@GetMapping(value="/accountantFirstView")
+	public List<RegistrationFrom> accountFirstView( @RequestParam(defaultValue = "0") int page,
+			 @RequestParam(defaultValue = "10") int size) {
+		return registrationService.accountFirstView(page, size);
+	}
+	
+	@GetMapping(value = "/downloadUserDocuments/{userId}")
+	public ResponseEntity getDocuments(@PathVariable String userId) throws UserNotFoundException, MalformedURLException {
+		return registrationService.getDocumentOfUser(userId);
+		
+	}
+	
+	@PostMapping(value="/uploadTranscationReceipt" ,  consumes = { "multipart/form-data" })
+	public ResponseEntity uploadTranscationRecepit(@RequestParam MultipartFile transcationRecepit) throws IOException, MessagingException {
+		return registrationService.uploadTranscationRecepit(getToken(), transcationRecepit);
+	}
+	
+	@GetMapping(value="/downloadPaymentReceipt/{userId}")
+	public ResponseEntity getPaymentReceipt(@PathVariable String userId) throws MalformedURLException {
+		return registrationService.getPaymentReceipt(userId);
+	}
 }
