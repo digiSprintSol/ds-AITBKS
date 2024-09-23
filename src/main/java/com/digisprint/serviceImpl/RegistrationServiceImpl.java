f4f11fd13590d4395e40922944cf937af9756df7
preeti2239 <prpal@digisprint.com>
2024-09-23 15:25:17 +0530

    code committing to prod

--- src/main/java/com/digisprint/serviceImpl/RegistrationServiceImpl.java
+++ src/main/java/com/digisprint/serviceImpl/RegistrationServiceImpl.java
@@ -168,10 +168,10 @@ public class RegistrationServiceImpl implements RegistrationService {
 				if (allUsersList.size() == 0) {
 					return new ResponseEntity("No data present", HttpStatus.NOT_FOUND);
 				} else {
-					return new ResponseEntity(allUsersList.reversed(), HttpStatus.OK);
+					return new ResponseEntity(allUsersList, HttpStatus.OK);
 				}
 			} else {
-				return new ResponseEntity(allUsersList.reversed(), HttpStatus.OK);
+				return new ResponseEntity(allUsersList, HttpStatus.OK);
 			}
 		}
 	}
