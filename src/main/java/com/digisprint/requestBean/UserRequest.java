18b8f51f12c66d9fe24a9440f8ceafa14d618cd8
Ganesh-digi <gmoyilla@digisprint.com>
2024-10-07 16:27:19 +0530

    chnaged userRequest Bean to RegistrationBean
    chnaged userRequest Bean to RegistrationBean

--- src/main/java/com/digisprint/requestBean/UserRequest.java
+++ src/main/java/com/digisprint/requestBean/UserRequest.java
@@ -9,12 +9,14 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.stereotype.Component;
 
 import com.digisprint.bean.Address;
 import com.digisprint.bean.FamilyDetails;
+import com.digisprint.bean.PaymentInfo;
 
 import lombok.Data;
 
@@ -22,33 +24,34 @@ import lombok.Data;
 @Component
 public class UserRequest {
 
+
 	/**
 	 * PERSONAL DETAILS
 	 */
-	@NotEmpty(message = "Please, do give your profile photo")
-	@NotNull(message = "Requried")
+	@NotEmpty(message="Please, do give your profile photo")
+	@NotNull(message="Requried")
 	private String profilePic;
 
 	@Indexed
-	@NotEmpty(message = "Don't pass an empty String")
-	@NotNull(message = "Requried")
+	@NotEmpty(message="Don't pass an empty String")
+	@NotNull(message="Requried")
 	private String firstName;
-
-	@NotEmpty(message = "Don't pass an empty String")
-	@NotNull(message = "Requried")
+	
+	@NotEmpty(message="Don't pass an empty String")
+	@NotNull(message="Requried")
 	private String lastName;
 
-	@NotNull(message = "Required")
-	@Past(message = "Date of birth must be in the past")
+	@NotNull(message="Required")
+	@Past(message="Date of birth must be in the past")
 	private LocalDateTime dateOfBirth;
 
-	@NotEmpty(message = "Don't pass an empty String")
-	@NotNull(message = "Requried")
-	private String gender; // {DROPDOWN}
+	@NotEmpty(message="Don't pass an empty String")
+	@NotNull(message="Requried")
+	private String gender; //{DROPDOWN}
 
-	@NotEmpty(message = "Don't pass an empty String")
-	@NotNull(message = "Requried")
-	private String category; // {DROPDOWN}
+	@NotEmpty(message="Don't pass an empty String")
+	@NotNull(message="Requried")
+	private String category; //{DROPDOWN}
 
 	/**
 	 * PRESENT ADDRESS
@@ -56,22 +59,22 @@ public class UserRequest {
 	private List<Address> address;
 
 	@Email
-	@NotEmpty(message = "Don't pass an empty String")
-	@NotNull(message = "Requried")
+	@NotEmpty(message="Don't pass an empty String")
+	@NotNull(message="Requried")
 	private String emailAddress;
 
-	@NotEmpty(message = "Don't pass an empty String")
-	@NotNull(message = "Requried")
-	@Size(min = 10, max = 10, message = "PhoneNumber")
+	@NotEmpty(message="Don't pass an empty String")
+	@NotNull(message="Requried")
+	@Size(min=10, max=10, message="PhoneNumber")
 	private String phoneNumber;
 
-	@NotEmpty(message = "Don't pass an empty String")
-	@NotNull(message = "Requried")
-	private String education;// {DROPDOWN}
+	@NotEmpty(message="Don't pass an empty String")
+	@NotNull(message="Requried")
+	private String education;//{DROPDOWN}
 
-	@NotEmpty(message = "Don't pass an empty String")
-	@NotNull(message = "Requried")
-	private String profession; // {DROPDOWN}
+	@NotEmpty(message="Don't pass an empty String")
+	@NotNull(message="Requried")
+	private String profession; //{DROPDOWN}
 
 	/**
 	 * FAMILY DETAILS
@@ -85,23 +88,89 @@ public class UserRequest {
 
 	private String voterIdCard;
 
-	@NotEmpty(message = "Don't pass an empty String")
-	@NotNull(message = "Requried")
+	@NotEmpty(message="Don't pass an empty String")
+	@NotNull(message="Requried")
 	private String occupation;
 
-	@NotEmpty(message = "Don't pass an empty String")
-	@NotNull(message = "Requried")
-	private String brieflyTellAboutYourself;
+	@NotEmpty(message="Don't pass an empty String")
+	@NotNull(message="Requried")
+	private String brieflyTellAboutYourself; 
 
-	@NotEmpty(message = "Don't pass an empty String")
-	@NotNull(message = "Requried")
-	private String reasonToJoinAITBKS;
+	@NotEmpty(message="Don't pass an empty String")
+	@NotNull(message="Requried")
+	private String reasonToJoinAITBKS; 
+
+	@NotEmpty(message="Don't pass an empty String")
+	@NotNull(message="Requried")
+	private String reference1;
 
 	private String categoryOfMembership;
 
-	private String nativePlace; 
+	/**
+	 * DECLERATION
+	 */
+	private boolean decalartionForRegOne;	
+
+	private String password;
+
+	private String confrimPassowrd;
+
+	@CreatedDate
+	private LocalDateTime createdDate;	
+
+	@Indexed
+	private LocalDateTime lastModifiedDate;
+
+	private String nativePlace; //R3
+
+	private String status; //from R1
+
+	private boolean decalarationForRegThree; //R3
+
+	private boolean isMemberOfOtherCommunity; //R3
+
+	private String communityName;
+
+	private PaymentInfo paymentInfo; //R3
+
+	/**
+	 * MEMBERSHIP INFO
+	 */
+
+	private String applicantChoosenMembership;
+
+	private String committeeOneChoosenMembershipForApplicant;
+
+	private String committeeTwoChoosenMembershipForApplicant;
+
+	private String committeeThreeChoosenMembershipForApplicant;
+
+	private String presidentChoosenMembershipForApplicant; //R2
+
+	private String presidentRemarksForApplicant; //R2
 	
-	private String casteStatus;
+	private String committeeOneApproval;
+
+	private String committeeTwoApproval;
+
+	private String committeeThreeApproval;	
+	
+	private String presidentApproval;
+	
+	private String committeeMemberOneId;
+	
+	private String committeeMemberTwoId;
+	
+	private String committeeMemberThreeId;
+	
+	private String presidentId;
+	
+	private String casteStatus; //oc/bc
+
+	/**
+	 * Membership id
+	 */
+	private boolean member;
 	
 	private String membershipId;
 
